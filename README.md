84873b37e2756eb9804e298ebb46bebb961fc720
kkyick2 <kkyick2@gmail.com>
2019-07-11 00:08:10 +0800

    20190711_0007
    20190711 tested python2.7 and python3.7 compatibility

---
+++ README.md
@@ -0,0 +1,17 @@
+# 201907_mscvelookup
+
+## requirement:
+- python3.7
+- requests
+- python2.7 (compatible)
+
+## usage - main.py
+1. Search microsoft CVE in march_cve.txt
+2. Lookup "https://portal.msrc.microsoft.com/api/security-guidance/en-US/CVE/CVE-2019-xxxx"
+
+## Create by
+- kkyick2
+
+## update
+- 20190708 draft
+- 20190711 tested python2.7 and python3.7 compatibility
--- main.py
+++ main.py
@@ -1,27 +1,46 @@
-import requests
-import pprint
-import time
+#!/usr/bin/env python
+# import pkg
 import csv
+import sys
+import pprint
+# import 3rd parties pkg
+import requests
 
-matchword = ['Windows 7 for 32-bit Systems Service Pack 1','Windows Server 2012 R2 (Server Core installation)']
+matchword = [
+	'Windows 7 for 32-bit Systems Service Pack 1',
+	'Windows Server 2012 R2 (Server Core installation)'
+]
+infilename = 'march_cve.txt'
+outfilename = 'output.csv'
 
-#open output CSV
-with open ('output.csv', mode='wb') as outF:
+
+# backwards compatibility for python3 and python2 csv writer
+def open_csv(filename, mode='w'):
+	"""
+	Open a csv file in proper mode depending on Python verion.
+	with open (outfilename, 'wb') as outF: 					# this code is for python2
+	with open (outfilename, 'w', newline='') as outF:	 	# this code is for python3
+	"""
+	return open(filename, mode=mode + 'b') if sys.version_info[0] == 2 else open(filename, mode=mode, newline='')
+
+
+# open output CSV
+with open_csv(outfilename, 'w') as outF:
 	writer = csv.writer(outF)
+	print('=========================================================================')
+	print('==================== Begin Script  ======================================')
+	print('=========================================================================')
+	count = 0
+	count1 = 0 # count for matched product
+	count2 = 0 # count for product not in use
 	# open CVE list
-	with open('march_cve.txt','r') as inF:
+	with open(infilename, 'r') as inF:
 		for cve in inF:
-			print('*******start *****************')
 			cve = cve.strip(' \n\t')
 			cve_url = "https://portal.msrc.microsoft.com/api/security-guidance/en-US/CVE/"+cve
-			print(cve_url)
+			print('=== Search web: {}'.format(cve_url))
 			response = requests.get(cve_url)
 			cve_dict = response.json()
-			#print('***************************')
-			#print(type(cve_dict[u'affectedProducts']))
-			print('***************************')
-			#print(len(cve_dict[u'affectedProducts']))
-			
 			outstring = ''
 			itemstring = ''
 			printflag = False
@@ -33,18 +52,26 @@ with open ('output.csv', mode='wb') as outF:
 						string1 = (cve_dict[u'affectedProducts'][i][u'name'])
 						string2 = (cve_dict[u'affectedProducts'][i][u'downloadTitle1']+":"+ cve_dict[u'affectedProducts'][i][u'articleTitle1'])
 						string3 = (cve_dict[u'affectedProducts'][i][u'downloadTitle2']+":"+ cve_dict[u'affectedProducts'][i][u'articleTitle2'])
-						itemstring = string1 +'\n' + string2 +'\n' + string3 +'\n'
+						itemstring = string1 + '\n' + string2 + '\n' + string3 + '\n'
 						printflag = True
 					else:
 						continue
 					outstring = outstring + itemstring
 			if printflag is True:
-				writer.writerow([cve,product,outstring])
+				writer.writerow([cve, product, outstring])
+				# print('=== Matched product')
+				count1 += 1
 			else:
-				writer.writerow([cve,'N/A','Affected product not used'])
-				
-			#time.sleep(1)
-			print('********* end *************')
-	
-	
-	
+				writer.writerow([cve, 'N/A', 'Affected product not in use'])
+				print('=== Affected product not in use')
+				count2 += 1
+
+			# End of this CVE link
+			count += 1
+			print('**********************')
+
+	print('=========================================================================')
+	print('=== Summary:')
+	print('=== Total CVE searched    : {}'.format(count))
+	print('=== No of Matched CVE     : {}'.format(count1))
+	print('=== No of not Affected CVE: {}'.format(count2))
